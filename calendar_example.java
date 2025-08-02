9f95c5b6c57ff3eab56596488c853cf8ba48821d
rahul2420030368 <2420030368@klh.edu.in>
2025-08-02 09:08:58 +0530

    Add files via upload

---
+++ calendar_example.java
@@ -0,0 +1,13 @@
+package MYPROJECT;
+import java.util.*;
+public class calendar_example {
+public static void main(String[] args) {
+	Calendar c = Calendar.getInstance();
+	Date d = c.getTime();
+	System.out.println(d);
+	c.set(2025,c.SEPTEMBER,13);			
+	int year =c.getWeekYear();
+	System.out.println(year);
+	int 
+}
+`
