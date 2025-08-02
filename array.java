19d6b5fd4978df5824070fc7d582f69fdba1781b
rahul2420030368 <2420030368@klh.edu.in>
2025-08-02 09:31:11 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,20 @@
+package MYPROJECT;
+
+public class array {
+	class Main {
+		 public static void main(String[] args) {
+		  
+	
+		   int[] age = {14, 9, 41, 2};
+
+		
+		   System.out.println("Accessing Elements of Array:");
+		   System.out.println("First Element: " + age[0]);
+		   System.out.println("Second Element: " + age[1]);
+		   System.out.println("Third Element: " + age[2]);
+		   System.out.println("Fourth Element: " + age[3]);
+		   System.out.println("Fifth Element: " + age[4]);
+		 }
+		}
+
+}
