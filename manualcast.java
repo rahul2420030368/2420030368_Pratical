65a6671220ad9dd8dfdaf3b15922cf5c4677e205
rahul2420030368 <2420030368@klh.edu.in>
2025-08-02 09:29:28 +0530

    Add files via upload

---
+++ manualcast.java
@@ -0,0 +1,12 @@
+package MYPROJECT;
+
+public class manualcast {
+	
+	  public static void main(String args[]) {
+	    double a=6.5;
+	    int b= (int)a;
+	    System.out.println(a);
+	    System.out.println(b);
+	  }
+	}
+
