129b048a3db69c7b974aa2c3b265b471e33dd644
rahul2420030368 <2420030368@klh.edu.in>
2025-08-02 09:27:52 +0530

    Add files via upload

---
+++ autocast.java
@@ -0,0 +1,15 @@
+package MYPROJECT;
+
+public class autocast {
+	
+	  public static void main(String args[]) {
+	    int a=5;
+	    double b=a;
+	    float c=a;
+	    System.out.println(a);
+	    System.out.println(b);
+	    System.out.println(c);
+	  }
+
+	}
+
